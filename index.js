6f3194bd7e58f20df71e1b3086def7f21a982287
lmorozco <lorozco@valizant.com>
2023-01-30 13:37:32 -0800

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
+const faker = require("faker");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
   res.send("Hola este es mi server en express");
 });
+
 app.get("/nueva-ruta", (req, res)=>{
   res.send("Hola, soy una nueva ruta");
 });
+
 app.get("/products", (req, res)=>{
-  res.json([{
-    name: "Product 1",
-    price: 1000,
-  },
-  {
-    name: "Product 2",
-    price: "2000",
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
   }
-]);
+  res.json(products);
 });
 
 app.get("/products/:id", (req, res) =>{
@@ -28,6 +35,18 @@ app.get("/products/:id", (req, res) =>{
     price: "2000",
   });
 });
+
+app.get("/users", (req, res) =>{
+  const {limit, offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset,
+    });
+  } else{
+    res.send("No hay parámetros");
+  }
+})
 
 app.get("/categories/:categoryId/products/:productId", (req, res) =>{
   const {categoryId, productId} = req.params;
