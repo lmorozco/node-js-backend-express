2b1aad4e67d721aacc6ee4ae68909312193f74b0
lmorozco <lorozco@valizant.com>
2023-01-31 11:06:02 -0800

    Separación de responsabilidades con ExpressRouter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const faker = require("faker");
+const routerApi = require("./routes");
+
 
 const app = express();
 const port = 3000;
@@ -12,48 +13,10 @@ app.get("/nueva-ruta", (req, res)=>{
   res.send("Hola, soy una nueva ruta");
 });
 
-app.get("/products", (req, res)=>{
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-//Todo lo que es específico (/products/filter), debe de ir antes de lo que es dinámico(/products/:id)
-app.get("/products/filter", (req, res) =>{
-  res.send("Yo soy un filter")
-});
+routerApi(app);
 
 
-app.get("/products/:id", (req, res) =>{
-  const {id} = req.params;
-  res.json({
-    id,
-    name: "Product 2",
-    price: "2000",
-  });
-});
 
-
-app.get("/users", (req, res) =>{
-  const {limit, offset} = req.query;
-  if(limit && offset){
-    res.json({
-      limit,
-      offset,
-    });
-  } else{
-    res.send("No hay parámetros");
-  }
-})
 
 app.get("/categories/:categoryId/products/:productId", (req, res) =>{
   const {categoryId, productId} = req.params;
---
+++ routes/products.router.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const faker = require("faker");
+
+const router = express.Router();
+
+router.get("/", (req, res)=>{
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+//Todo lo que es específico (/products/filter), debe de ir antes de lo que es dinámico(/products/:id)
+router.get("/filter", (req, res) =>{
+  res.send("Yo soy un filter")
+});
+
+
+router.get("/:id", (req, res) =>{
+  const {id} = req.params;
+  res.json({
+    id,
+    name: "Product 2",
+    price: "2000",
+  });
+});
+
+
+module.exports = router;
