69f198dc724372536651dc92c799edcecc0db798
lmorozco <lorozco@valizant.com>
2023-01-31 15:58:10 -0800

    Códigos de estado o HTTP response status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -41,4 +41,23 @@ router.post("/", (req, res) =>{
   })
 });
 
+router.patch("/:id", (req, res) =>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: "update",
+    data: body,
+    id,
+  });
+});
+
+router.delete("/:id", (req, res) =>{
+  const {id} = req.params;
+  res.json({
+    message: "deleted",
+    data: body,
+    id,
+  });
+});
+
 module.exports = router;
