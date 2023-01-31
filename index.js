082c80497c51c23aa9bb5fbe9350c49999093cf6
lmorozco <lorozco@valizant.com>
2023-01-31 15:17:47 -0800

    POST: método para crear [clase 10]

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const routerApi = require("./routes");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res)=>{
   res.send("Hola este es mi server en express");
 });
@@ -14,17 +16,6 @@ app.get("/nueva-ruta", (req, res)=>{
 });
 
 routerApi(app);
-
-
-
-
-app.get("/categories/:categoryId/products/:productId", (req, res) =>{
-  const {categoryId, productId} = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
 
 app.listen(port, ()=>{
   console.log("Mi port es " + port);
